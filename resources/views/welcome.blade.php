e6f5fa91aebc6aff2125f37764a59aed853f9133
Nicole Ashley Ancheta <anchetanicole1020@gmail.com>
2026-08-18 08:29:59 +0800

    feat: initialize Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello Laravel</title>
+</head>
+<body>
+    <h1>Hello Laravel!</h1>
+    <h2>Welcome to Client-Server Technologies</h2>
+
+    <p><strong>Student Name:</strong> Nicole Ashley T. Ancheta</p>
+    <p><strong>Student Number:</strong> 0124-1078</p>
+    <p><strong>Course:</strong> BSIT</p>
+    <p><strong>Section:</strong> 3C</p>
+    <p><strong>Subject:</strong> Client-Server Technologies</p>
+    <p><strong>Current Date:</strong> August 18, 2026</p>
+</body>
+</html>
